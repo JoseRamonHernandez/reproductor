54a178cdfd8e8d5a89ca7d62cb95f3829d03c77e
José Ramón Hernández Muñoz <joseramonhernandezmunoz20@gmail.com>
2022-02-12 14:55:23 -0600

    new Project

---
+++ js/index.js
@@ -0,0 +1,16 @@
+// const pausa = document.getElementById('Pause');
+
+// const accion = document.addEventListener('click', pausa)
+// {
+//     console.log('haz dado click en pausa');
+// }
+//console.log('Happy hacking :) xd')
+
+//var video = document.getElementById("videoPlayer");
+
+ const reproducir = document.getElementById('reproducir');
+ const accion = () =>{
+     // video.play();
+     console.log("haz dado click en reproducir");
+ }
+ reproducir.addEventListener('click', accion);
